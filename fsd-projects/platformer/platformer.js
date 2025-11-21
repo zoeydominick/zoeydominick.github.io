ffb1c952e133956cb3f9395005b9be6cdf827000
zoeydominick <zoey.dominick@inspire35.org>
2025-11-21 21:25:20 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -34,20 +34,24 @@ $(function () {
     createPlatform(189,700,200,15,"hotpink");
     createPlatform(425,569,200,14,"babypink");
     createPlatform(710,620,200,12,"hotpink");
-    createPlatform(978,578,250,12,"babypink")
+    createPlatform(978,578,250,12,"babypink");
+    createPlatform(1009,645,300,13,"hotpink");
+
 
 
 
     // TODO 3 - Create Collectables
 
     createCollectable("database",360,170,0.5,0.7); 
+    createCollectable("database",450,230,0.5,0.7);
+    createCollectable("database",528,298,0.5,0.7);
 
     
     // TODO 4 - Create Cannons
 
 createCannon("top",307,1000);
 createCannon("top",567,1000);
-
+createCannon("top",679,1000);
 
     
     
